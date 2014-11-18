b334b6ed64ea78e719c4d9593518507041edfc12
ahmed-hamdy90 <ahmedhamdy20@gmail.com>
2014-11-19 00:13:57 +0200

    reformat code in server.js file

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 /**
- * @requires express package
- * @requires stylus  package
- * @requires morgon  package
- * @requires mongoose package
+ * require public modules
+ * @requires express module
+ * @requires stylus module
+ * @requires morgon module
+ * @requires mongoose module 
  */
 var express  = require('express'),
-	stylus   = require('stylus'),
-	morgan   = require('morgan'),
-	mongoose = require('mongoose');
+    stylus   = require('stylus'),
+    morgan   = require('morgan'),
+    mongoose = require('mongoose');
 
 // set environment mode value for node application and default value must be development
 // env value will contains process.env.NODE_ENV value or default value (development)
@@ -25,13 +26,13 @@ mongoose.connect("mongodb://localhost/multivision");
  * @type {object}
  */
 var db = mongoose.connection;
-	// create listener to log errors if happened when connect to mongodb
-	db.on('error', console.error.bind(console, "connection error: ") );
-	// create listener when connection to mongodb opened
-	db.once('open', function callback () {
+    // create listener to log errors if happened when connect to mongodb
+    db.on('error', console.error.bind(console, "connection error: ") );
+    // create listener when connection to mongodb opened
+    db.once('open', function callback () {
 
-		console.log("connection to multivision database is opened");
-	});
+		   console.log("connection to multivision database is opened");
+	  });
 // create Schema for message , massage Schema define the shape of the message document
 // i.e we define properties with properties type for message document
 var messageSchema = mongoose.Schema({
@@ -83,47 +84,43 @@ Message.findOne({},function (err, message) {
  * @type {object}
  */
 var app = express();
-	// set configuration for path of views directory
-	app.set('views', __dirname + '/server/views');
-	// set view engine which will used in Node Application
-	app.set('view engine', 'jade');
-	// use morgan middleware to log info into terminal
-	app.use(morgan('dev'));
-	// use stylus middleware
-	app.use(stylus.middleware(
-		{
-			// set path for source file
-			src : __dirname + '/public',
-			// set compile function for stylus
-			compile : function (str, path) {
-				return stylus(str)
-					.set('filename', path);
-			}
+    // set configuration for path of views directory
+    app.set('views', __dirname + '/server/views');
+    // set view engine which will used in Node Application
+    app.set('view engine', 'jade');
+    // use morgan middleware to log info into terminal
+    app.use(morgan('dev'));
+    // use stylus middleware
+    app.use(stylus.middleware({
+		   // set path for source file
+		   src : __dirname + '/public',
+		   // set compile function for stylus
+		   compile : function (str, path) {
+			    return stylus(str)
+				  .set('filename', path);
+	 	   }
+	  }));
+    // use express.static to enable express to look into public directory
+    // to enable route to get images , styles and javascript files
+    app.use(express.static(__dirname + '/public'));
+    /**
+     * set route of Node Application [any route]
+     *
+     * @param {object} req request object which will come from this route
+     * @param {object} res response object which will come from this route
+     */
+     app.get('/partials/:partialPath', function (req, res) {
 
-		}
-	));
-	// use express.static to enable express to look into public directory
-	// to enable route to get images , styles and javascript files
-	app.use(express.static(__dirname + '/public'));
-	/**
-	 * set route of Node Application [any route]
-	 *
-	 * @param {object} req request object which will come from this route
-	 * @param {object} res response object which will come from this route
-	 */
-	app.get('/partials/:partialPath', function (req, res) {
+	      res.render('partials/' + req.params.partialPath);
+     });
+     app.get('*', function(req, res) {
 
-		res.render('partials/' + req.params.partialPath);
-	});
-	app.get('*', function(req, res) {
-
-		res.render('index',{
-				mongoMessage : savedMessageObject.message
-		});
-
-	});
-	// set port number for Node Application
-	app.listen(3030);
+	      res.render('index',{
+			     mongoMessage : savedMessageObject.message
+	      });
+     });
+     // set port number for Node Application
+     app.listen(3030);
 
 // create a message will display into console when we run server.js file
 console.log("MultiVision Application Listening on port 3030");
